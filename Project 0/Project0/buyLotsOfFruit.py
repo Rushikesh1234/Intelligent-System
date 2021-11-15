a86ba56d6f85b3cfc1ac2205b43b4db19a864b26
Rushikesh Machhindra Khamkar <38386736+Rushikesh1234@users.noreply.github.com>
2021-11-15 15:06:01 -0600

    Add files via upload

---
+++ Project 0/Project0/buyLotsOfFruit.py
@@ -0,0 +1,68 @@
+# buyLotsOfFruit.py
+# -----------------
+# Licensing Information:  You are free to use or extend these projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
+# 
+# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
+# The core projects and autograders were primarily created by John DeNero
+# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+# Student side autograding was added by Brad Miller, Nick Hay, and
+# Pieter Abbeel (pabbeel@cs.berkeley.edu).
+
+
+"""
+To run this script, type
+
+  python buyLotsOfFruit.py
+
+Once you have correctly implemented the buyLotsOfFruit function,
+the script should produce the output:
+
+Cost of [('apples', 2.0), ('pears', 3.0), ('limes', 4.0)] is 12.25
+"""
+from __future__ import print_function
+
+fruitPrices = {'apples': 2.00, 'oranges': 1.50, 'pears': 1.75,
+               'limes': 0.75, 'strawberries': 1.00}
+
+
+def buyLotsOfFruit(orderList):
+    """
+        orderList: List of (fruit, numPounds) tuples
+
+    Returns cost of order
+    """
+    totalCost = 0.0
+    flag = 0
+    "*** TTU CS 5368 YOUR CODE HERE ***"
+
+    for item in orderList:
+        flag = 0;
+        fruit_name = item[0];
+        fruit_count = item[1];
+
+        #print("Order_List",fruit_name," ",fruit_count)
+
+        for item1 in fruitPrices:
+            name = item1;
+            amount = fruitPrices[name];
+
+            #print("Price_List",name," ",amount)
+            
+            if(name == fruit_name):
+                totalCost = totalCost + (fruit_count * amount)
+                flag = 1
+
+        if(flag == 0):
+            return 0.0
+    
+    return totalCost
+
+
+# Main Method
+if __name__ == '__main__':
+    "This code runs when you invoke the script from the command line"
+    orderList = [('apples', 2.0), ('pears', 3.0), ('limes', 4.0)]
+    print('Cost of', orderList, 'is', buyLotsOfFruit(orderList))
